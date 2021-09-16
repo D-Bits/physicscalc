912b65b31ddd94e8e89104671df06decad751fa9
D-Bits <lockwood.da88@gmail.com>
2021-09-16 12:15:50 -0700

    Added gravForce() function.

--- grav.go
+++ grav.go
@@ -1,1 +1,14 @@
 package main
+
+import "math"
+
+func gravForce(m1 float64, m2 float64, r float64) float64 {
+
+	// Newton's constant
+	G := 6.674 * (math.Pow(10, -11))
+
+	formula := G * (m1 * m2 / r)
+
+	return math.Round(formula)
+
+}
